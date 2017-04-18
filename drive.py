aec1244ad1d02aa45731cfbd7ff6bc8fefe3c1f7
Liam <liam.bowers@adp.com>
2017-04-18 15:08:16 -0400

    update drive.py

--- drive.py
+++ drive.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
+import cv2
 
 import numpy as np
 import socketio
@@ -44,9 +45,13 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 20
 controller.set_desired(set_speed)
 
+def preprocess_img(img):
+    """ minimize variance in light and shadow by using histogram equalization
+    """
+    return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -60,7 +65,7 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+        image_array = preprocess_img(np.array(image))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
